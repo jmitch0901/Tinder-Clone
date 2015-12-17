9efef431f45f2df67b02030a8af838037d9f0fdd
jonathanmitchell <jmitch0901@gmail.com>
2015-12-16 23:44:12 -0600

    Got basic me requests done!

--- Tinder Clone/ViewController.swift
+++ Tinder Clone/ViewController.swift
@@ -10,17 +10,74 @@ import UIKit
 import FBSDKCoreKit
 import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    @IBOutlet weak var loginButton: FBSDKLoginButton!
+    
+    let loginManager = FBSDKLoginManager()
+
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!)
+    {
+        if error != nil {
+            print("There was an error logging in:")
+            if let error = error {
+                print(error)
+            }
+            return
+        }
+        
+        if let result = result{
+            //print(result)
+            //print("Granted permissions: \(result.grantedPermissions)")
+            
+            if result.declinedPermissions.count > 0 {
+                print("Declined permissions: \(result.declinedPermissions)")
+            }
+            
+            self.makeMeRequest()
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!)
+    {
+        print("Bye bye!")
+    }
+    
+    func makeMeRequest(){
+        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields":"id,name,email,picture"])
+            .startWithCompletionHandler({ (connection, anyObject, error) -> Void in
+                
+                if error != nil{
+                    print("There was an error while querying facebook! \(error)")
+                    return
+                }
+                
+                //print(connection)
+                print(anyObject)
+            })
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let fbButton = FBSDKLoginButton()
-        fbButton.center = self.view.center
-        self.view.addSubview(fbButton)
+        loginButton.delegate = self
+        loginButton.readPermissions = ["user_friends","email","public_profile"]
+        
+       
+        if FBSDKAccessToken.currentAccessToken() != nil {
+            print("You're already logged in")
+            self.makeMeRequest()
+        }
+        
+        
         
         
         
     }
+    
+  
+    
+    
 }
 
